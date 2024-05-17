74db6f11c8d80fe434d501da236398a4e31c1e0e
wp043 <22.wendy.pan@gmail.com>
2024-05-17 09:57:12 -0400

    burger menu animation

--- script.js
+++ script.js
@@ -3,4 +3,10 @@ function toggleMenu() {
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+
+    if (menu.classList.contains("open")) {
+        document.body.style.overflow = "hidden";
+    } else {
+        document.body.style.overflow = "auto";
+    }
 }
